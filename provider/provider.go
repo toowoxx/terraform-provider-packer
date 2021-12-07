3d96d7fcecfdb87827f4d22293aed00fc26ac186
Simão Gomes Viana <devel@superboring.dev>
2021-12-07 20:15:01 +0100

    provider: add Description to dummy
    Signed-off-by: Simao Gomes Viana <simao.gomes@toowoxx.de>

--- provider/provider.go
+++ provider/provider.go
@@ -24,9 +24,10 @@ func (p *provider) GetSchema(_ context.Context) (tfsdk.Schema, diag.Diagnostics)
 	return tfsdk.Schema{
 		Attributes: map[string]tfsdk.Attribute{
 			"dummy": {
-				Type:     types.StringType,
-				Optional: true,
-				Computed: true,
+				Type:        types.StringType,
+				Optional:    true,
+				Computed:    true,
+				Description: "Used as a placeholder. Do not use.",
 			},
 		},
 	}, nil
